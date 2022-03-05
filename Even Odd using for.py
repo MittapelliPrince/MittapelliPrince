cd9460935267fcd813f4693a9b074d708318157f
MittapelliPrince <100847546+MittapelliPrince@users.noreply.github.com>
2022-03-05 17:46:30 +0530

    Create Even Odd using for.py

---
+++ Even Odd using for.py
@@ -0,0 +1,14 @@
+# Even Numbers using for loop
+x = int(input('Enter the No. Values: '))
+
+for x in range(1, x+1):
+  if x % 2 == 0:
+    print(x)
+    
+# Odd numbers using for loop
+x = int(input('Enter the No. Values: '))
+
+for x in range(1, x+1):
+  if x % 2 != 0:
+    print(x)
+ 
