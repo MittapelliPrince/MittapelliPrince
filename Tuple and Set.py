4af593f4be65cdcb0b2b749ff8510f213fe662ca
MittapelliPrince <100847546+MittapelliPrince@users.noreply.github.com>
2022-03-05 10:57:08 +0530

    Create Tuple and Set.py

---
+++ Tuple and Set.py
@@ -0,0 +1,20 @@
+tup = (98, 67, 34, 65)
+print(tup)
+
+# counting the value
+print(tup.count(98))
+
+# fetching the index number
+print(tup.index(65))
+
+# Sets
+set = {34, 65, 98, 21, 90}
+print(set)
+
+# append or add the number
+set.add(43)
+print(set)
+
+# pop's the first value
+set.pop()
+print(set)
