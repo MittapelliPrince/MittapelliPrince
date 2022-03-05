7782a7f55181c41f1df30074342f81f406161216
MittapelliPrince <100847546+MittapelliPrince@users.noreply.github.com>
2022-03-05 10:23:04 +0530

    Create List.py

---
+++ List.py
@@ -0,0 +1,35 @@
+list = [32,54,12,54,65]
+print(list)
+
+# finding index number
+print(list.index(54))
+
+# append the number
+list.append(88)
+print(list)
+
+# inserting the value in particular index
+list.insert(3, 47)
+print(list)
+
+# extend or adding the no of values
+list.extend([92, 22, 64])
+print(list)
+
+# counting the values
+print(list.count(54))
+
+# removing the value
+list.remove(54)
+print(list)
+
+# pop the value
+print(list.pop())
+
+# sorting the list
+list.sort()
+print(list)
+
+# reverse the list
+list.reverse()
+print(list)
